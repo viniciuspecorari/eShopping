4f9bd1947c452976ea6e99a7e5ff2a14bc749c03
Vinicius Pecorari <ovinicius.pecorari@gmail.com>
2024-05-02 21:10:43 -0300

    Adicionar arquivos de projeto.

---
+++ Catalog/Catalog.API/Startup.cs
@@ -0,0 +1,34 @@
+﻿namespace Catalog.API
+{
+    public class Startup
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+           _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseStaticFiles();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();                
+            });
+        }
+    }
+}
