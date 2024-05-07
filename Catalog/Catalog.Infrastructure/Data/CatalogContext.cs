8a7c871d585a7f8b61eb249a824fa538e68e721b
Vinicius Pecorari <ovinicius.pecorari@gmail.com>
2024-05-06 21:10:47 -0300

    configuração do contexto e dados inciais no catalogo

---
+++ Catalog/Catalog.Infrastructure/Data/CatalogContext.cs
@@ -0,0 +1,32 @@
+﻿using Catalog.Core.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Infrastructure.Data;
+
+public class CatalogContext : ICatalogContext
+{
+    public IMongoCollection<Product> Products { get; }
+    public IMongoCollection<ProductBrand> Brands { get; }
+    public IMongoCollection<ProductType> Types { get; }
+
+    public CatalogContext(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration.GetValue<string>("DataBaseSettings:ConnectionString"));
+        var database = client.GetDatabase(configuration.GetValue<string>("DataBaseSettings:DataBaseName"));
+        Brands = database.GetCollection<ProductBrand>(configuration.GetValue<string>("DataBaseSettings:BrandsCollection"));
+        Types = database.GetCollection<ProductType>(configuration.GetValue<string>("DataBaseSettings:TypesCollection"));
+        Products = database.GetCollection<Product>(configuration.GetValue<string>("DataBaseSettings:CollectionName"));
+
+        BrandContextSeed.SeedData(Brands);
+        TypeContextSeed.SeedData(Types);        
+        CatalogContextSeed.SeedData(Products);
+    }
+
+}
+
---
+++ Catalog/Catalog.Infrastructure/Data/ICatalogContext.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace Catalog.Infrastructure.Data
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+        IMongoCollection<ProductBrand> Brands { get; }
+        IMongoCollection<ProductType> Types { get; }
+    }
+}
